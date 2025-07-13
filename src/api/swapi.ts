a944e5513214a89657c8d5aa1e6ff2d505f3be35
Tristan <tristan.raissherman@gmail.com>
2025-07-13 09:39:51 -0400

    feat: add data types and fetching

--- src/api/swapi.ts
+++ src/api/swapi.ts
@@ -1,8 +1,17 @@
-import type { Film } from "../types/swapi"
+import type { Film, Person, Planet } from "../types/swapi"
 import axios from 'axios'
 
 export const fetchFilms = async (): Promise<Film[]> => {
     const { data } = await axios.get('https://swapi.info/api/films')
-    console.log(data)
+    return data.results
+}
+
+export const fetchPeople = async (): Promise<Person[]> => {
+    const { data } = await axios.get('https://swapi.info/api/people')
+    return data.results
+}
+
+export const fetchPlanets = async (): Promise<Planet[]> => {
+    const { data } = await axios.get('https://swapi.info/api/planets')
     return data.results
 }
