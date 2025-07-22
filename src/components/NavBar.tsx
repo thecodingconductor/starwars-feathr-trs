b68087c84ae61763c5e664806b0fbb75c17a9fd1
Tristan <tristan.raissherman@gmail.com>
2025-07-22 13:41:45 -0400

    fix: fixed navbar issue

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -78,7 +78,13 @@ const Overlay = styled(Dialog.Overlay)`
 
 const MenuContent = styled(Dialog.Content)`
   position: fixed;
-  inset: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  padding: 2rem;
   background: #111;
   display: flex;
   flex-direction: column;
